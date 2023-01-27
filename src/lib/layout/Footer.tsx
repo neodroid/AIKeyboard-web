1f8896719e61b16ee2d6e740b39f6e6342a41721
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-01-27 18:28:13 +0700

    first commit

---
+++ src/lib/layout/Footer.tsx
@@ -0,0 +1,16 @@
+import { Flex, Link, Text } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Flex as="footer" width="full" justifyContent="center">
+      <Text fontSize="sm">
+      ©{new Date().getFullYear()} - AI Keyboard by{" "}
+        <Link href="https://twitter.com/kevinahmad" isExternal rel="noopener noreferrer" textDecoration={'underline'}>
+          Kevin
+        </Link>
+      </Text>
+    </Flex>
+  );
+};
+
+export default Footer;
