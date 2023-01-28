b8bf72eadeb0c1c886ed967e2176c56aaad68583
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-01-29 01:22:56 +0700

    feat(pages): home

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,22 +1,37 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <Flex
       direction="column"
-      alignItems="center"
-      justifyContent="center"
+      // alignItems="center"
+      // justifyContent="center"
       minHeight="70vh"
       gap={4}
       mb={8}
       w="full"
+      // bg="red"
     >
-      <NextSeo title="Home" />
-      <Text>Hello makasih udh mampir mas bro</Text>
-      {/* <SomeText />
-      <SomeImage />
-      <CTASection /> */}
+      <NextSeo title="AI Keyboard" />
+      <Flex alignItems="center" direction="column">
+        <Text fontSize="4xl" fontWeight="bold">
+          AI Keyboard
+        </Text>
+        <Text fontSize="xl" fontWeight="regular">
+          Generate anything at you keyboard
+        </Text>
+        <Image src="demo.gif" borderRadius="20" maxW="90%" mt="50" />
+        <Flex mt="50" justifyContent="space-evenly">
+          <Flex mr="10">
+            <Link href="/terms">Terms and conditions</Link>
+          </Flex>
+          <Flex>
+            <Link href="/privacy">Privacy Policy</Link>
+          </Flex>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
