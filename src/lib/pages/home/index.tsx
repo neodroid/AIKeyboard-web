78d218895d5c20ab7a824230e1a7ea390fa7f7fb
Kevin Ahmad <kevinahmad.f@gmail.com>
2023-02-01 13:15:08 +0700

    feat(components): add waitlist

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,38 +1,119 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  Icon,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { NextSeo } from "next-seo";
 import Link from "next/link";
+import type { SetStateAction } from "react";
+import { useRef, useState } from "react";
+// import Banner from "../../components/Banner"
+import { FiInfo } from "react-icons/fi";
 
 const Home = () => {
+  const initialRef = useRef(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [userEmail, setUserEmail] = useState("");
+  const handleChange = (event: { target: { value: SetStateAction<string> } }) =>
+    setUserEmail(event.target.value);
+  const handleClick = async () => {
+    try {
+      await fetch("https://callisto-api.vercel.app/api/waitlist", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: userEmail }),
+      });
+      // const data = await response.json();
+      // console.log(data);
+    } catch (error) {
+      // console.error(error);
+    }
+    onClose();
+  };
   return (
-    <Flex
-      direction="column"
-      // alignItems="center"
-      // justifyContent="center"
-      minHeight="70vh"
-      gap={4}
-      mb={8}
-      w="full"
-      // bg="red"
-    >
-      <NextSeo title="AI Keyboard" />
-      <Flex alignItems="center" direction="column">
-        <Text fontSize="4xl" fontWeight="bold">
-          AI Keyboard
-        </Text>
-        <Text fontSize="xl" fontWeight="regular">
-          Generate anything at you keyboard
-        </Text>
-        <Image src="demo.gif" borderRadius="20" maxW="90%" mt="50" />
-        <Flex mt="50" justifyContent="space-evenly">
-          <Flex mr="10">
-            <Link href="/terms">Terms and conditions</Link>
+    <>
+      {/* <Banner/> */}
+      <Flex
+        direction="column"
+        // alignItems="center"
+        // justifyContent="center"
+        minHeight="70vh"
+        gap={4}
+        mb={8}
+        w="full"
+        // bg="red"
+      >
+        <NextSeo title="AI Keyboard" />
+        <Flex alignItems="center" direction="column">
+          <Text fontSize="4xl" fontWeight="bold">
+            AI Keyboard
+          </Text>
+          <Text fontSize="xl" fontWeight="light" textAlign="center">
+            Generate anything at your keyboard
+          </Text>
+          <Flex mt="10">
+            <Icon as={FiInfo} boxSize="6" />
+            <Text textAlign="center">
+              We are currently on Apple Appstore submission review!
+            </Text>
           </Flex>
-          <Flex>
-            <Link href="/privacy">Privacy Policy</Link>
+          <Button mt="5" p="7" onClick={onOpen}>
+            Add me to the waitlist!
+          </Button>
+          <Text fontWeight="hairline" mt="5" textAlign="center">
+            We will give you an email notification when the keyboard is live if
+            you're on the waitlist!
+          </Text>
+          <Image src="demo.gif" borderRadius="20" maxW="90%" mt="50" />
+          <Flex mt="50" justifyContent="space-evenly">
+            <Flex mr="10">
+              <Link href="/terms">Terms and Conditions</Link>
+            </Flex>
+            <Flex>
+              <Link href="/privacy">Privacy Policy</Link>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-    </Flex>
+      <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Enter your email</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Input
+                value={userEmail}
+                onChange={handleChange}
+                ref={initialRef}
+                placeholder="email"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClick}>
+              Submit
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
